5ae2f910275bf03017b033a150004bcbe8c0af94
Rodney Kahane <rodney@rodneys-macbook-pro.local>
2017-11-27 18:45:22 -0500

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Rodney Kahane on 11/27/17.
+//  Copyright © 2017 Rodney Kahane. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
